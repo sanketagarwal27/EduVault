412f1487c8d628a6e5dfebdb5d6877102fb45d8a
Sanket Agarwal <sanketagarwal314@gmail.com>
2025-10-04 02:16:17 +0530

    Updated routes import and declaration

--- src/app.js
+++ src/app.js
@@ -3,7 +3,10 @@ import cors from "cors"
 import cookieParser from "cookie-parser";
 
 //routes import
-
+import studentRouter from "./routes/student.routes.js";
+import facultyRouter from "./routes/faculty.routes.js";
+import institutionRouter from "./routes/institution.routes.js";
+import certificationRouter from "./routes/certification.routes.js";
 
 const app = express();
 
@@ -24,6 +27,9 @@ app.use(cookieParser())
 
 
 //routes declaration
-
+app.use("/EduVault/v1/student", studentRouter);
+app.use("/EduVault/v1/faculty", facultyRouter);
+app.use("/EduVault/v1/institution", institutionRouter)
+app.use("/EduVault/v1/certification", certificationRouter);
 
 export {app};
