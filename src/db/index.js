79ce13a51efcf3ddec22e6997d17fa91f83b9f81
Sanket Agarwal <sanketagarwal314@gmail.com>
2025-10-04 01:56:42 +0530

    Added the database connection

---
+++ src/db/index.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+export const connectDB = async () => {
+    try{
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}${DB_NAME}`);
+        console.log(`MONGODB connected at host: ${connectionInstance.connection.host}`);
+    }
+    catch(err){
+        console.log("MONGODB connection error: ", err);
+        throw err;
+    }
+}
